b0d043e6923a3f3de71d95ff95af1ac05046a245
ddaog2 <ktu6593@naver.com>
2026-02-01 13:05:25 +0900

    Trigger deployment (v2.1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+// Deployment trigger: v2.1
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
